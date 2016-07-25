7326617242e9343c51b3cf4ac21d4babf6c28040
pingpingg <pingpingg@jumei.com>
2016-07-25 10:50:48 +0800

    commit

---
+++ src/net/fs/client/ClientUII.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2015 D1SM.net
+
+package net.fs.client;
+
+public interface ClientUII {
+
+
+
+
+	boolean login();
+
+	boolean updateNode(boolean testSpeed);
+	
+	boolean isOsx_fw_pf();
+	
+	 boolean isOsx_fw_ipfw();
+
+}
